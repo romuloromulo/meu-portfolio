3b894b5abd1691dcbf622bd26d2dec890608a74f
Rômulo Viana <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2023-09-23 13:16:27 -0300

    att

--- src/Sections/Sobre.js
+++ src/Sections/Sobre.js
@@ -14,19 +14,18 @@ function Sobre() {
         <div className="w-4/5 flex flex-col justify-center items-center mt-10 group">
           <div className="border p-2 sm:p-8 md:p-10  lg:p-14  rounded-lg border-cool-black-300  ">
             <p className="text-dim-gray md:text-base text-sm ">
-              Olá! Meu nome é <b>Rômulo</b>, e sou um apaixonado desenvolvedor
+              Olá! Meu nome é <b>Rômulo</b>, sou um apaixonado desenvolvedor
               front-end com um pouco mais de um ano de experiência no mundo da
               programação. Durante minha jornada, mergulhei profundamente em
               tecnologias modernas, aprimorando minhas habilidades em Next.js,
-              React e React Native. Minha busca incessante pelo conhecimento me
-              leva a estudar constantemente e buscar maneiras de aprimorar
-              minhas habilidades. Sou movido pela minha curiosidade que me
-              impulsiona a criar soluções elegantes e funcionais para os
-              desafios que encontro. Minha maior motivação é continuar evoluindo
-              até me tornar um desenvolvedor pleno. Estou entusiasmado com as
-              oportunidades que o futuro reserva e ansioso para enfrentar novos
-              desafios enquanto continuo minha jornada no desenvolvimento
-              front-end.
+              React e React Native. Busco sempre estudar constantemente e buscar
+              maneiras de aprimorar minhas habilidades. Sou movido pela minha
+              curiosidade que me impulsiona a criar soluções elegantes e
+              funcionais para os desafios que encontro. Minha maior motivação é
+              continuar evoluindo até me tornar um desenvolvedor pleno. Estou
+              entusiasmado com as oportunidades que o futuro reserva e ansioso
+              para enfrentar novos desafios enquanto continuo minha jornada no
+              desenvolvimento front-end.
             </p>
           </div>
         </div>
