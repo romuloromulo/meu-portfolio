40c838a17e04a33db2a45d9dcd3cfab9d992322a
romuloromulo <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2024-02-15 18:22:42 -0300

    consertando pequeno bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Header from "./sections/Header";
 import Sobre from "./sections/Sobre";
 import Habilidades from "./sections/Habilidades";
 import Projetos from "./sections/Projetos";
 import { BrowserRouter } from "react-router-dom";
 import Formulario from "./sections/Formulario";
 import AppFooter from "./sections/Footer";
+import Header from "./Sections/Header";
 
 function App() {
   return (
